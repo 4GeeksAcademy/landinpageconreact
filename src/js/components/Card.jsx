726ab55f51fceb23c06a81f97bef8298d6e77f3e
Manuel Pernia <manuelk211995@gmail.com>
2026-03-04 12:12:55 +0000

    proyect reac done

---
+++ src/js/components/Card.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+function Card() {
+
+  return (
+
+    <>
+
+      <div className="col-">
+        <div className="card h-100">
+          <img src="..." className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <button type="button" className="btn btn-primary">Primary</button>
+          </div>
+        </div>
+      </div>
+
+    </>
+    )
+
+}
+
+export default Card;
--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,24 @@
 import React from "react";
+import Navbar from "./Navbar";
+import Jumbotron from "./Jumbotron";
+import Card from "./Card";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
+
+
 const Home = () => {
 	return (
 		<div className="text-center">
-            
-
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<Navbar />
+			<Jumbotron />
+			<div className="row row-cols-1 row-cols-md-4 g-4 mt-5">
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+				
+				</div>
+			
 		</div>
 	);
 };
